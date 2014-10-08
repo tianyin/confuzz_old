cddbbadde5d1dc5e0103a556734cd3510db64e6c
Tianyin Xu <tixu@cs.ucsd.edu>
2014-10-07 22:25:21 -0700

    remove the cache at the beginning of each start

--- squid/squidmgr.sh
+++ squid/squidmgr.sh
@@ -6,6 +6,7 @@ LOGDIR=$ROOTDIR/var/logs
 if [ "$1" = "start" ]; then
     rm -f $LOGDIR/access.log
     rm -f $LOGDIR/cache.log
+    rm -Rf $ROOTDIR/var/cache/squid
     $ROOTDIR/sbin/squid -f $ROOTDIR/etc/squid.conf -d 1 -N >$ROOTDIR/stdout.log 2>$ROOTDIR/stderr.log &
     echo "Started..."
 
